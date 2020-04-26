28d5b1c6fe330a171b7c1ff82e209e881ea6e414
Ekaterina <55707945+Rocknroll-nerd@users.noreply.github.com>
2020-04-26 05:49:00 +0500

    Update bfs_graph.py

--- bfs_graph.py
+++ bfs_graph.py
@@ -1,6 +1,5 @@
 from queue import Queue
 
-
 def bfs_check(start=0):
     global visited
     q = Queue(len(gr))
@@ -12,23 +11,21 @@ def bfs_check(start=0):
             if visited[vertex] == 0:
                 visited[vertex] = -1*visited[node]
                 q.put(vertex)
+                continue
             elif visited[vertex] == visited[node]:
-                print("Graph is not bipartite")
                 return "Graph is not bipartite"
-        while True:
-            for ind, i in enumerate(visited):
-                if i == 0:
-                    start = ind
+            break
+            for vertex in range(len(visited)):
+                if vertex == 0:
+                    start = vertex
                     bfs_check(start)
                     break
-            break
     return "Graph is bipartite"
 
 
 if __name__ == "__main__":
     #gr = [[1, 2], [2, 3, 4], [], [], []]
-    gr = [[1, 2], [3, 4], [], [], [], [6], [7], [5]]
-    #gr = [[1, 2], [3, 4], [], [], [], [6], [], []]
+    #gr = [[1, 2], [3, 4], [], [], [], [6], [7], [5]]
+    gr = [[1, 2], [3, 4], [], [], [], [6], [], []]
     visited = [0] * len(gr)
     print(bfs_check(0))
-
